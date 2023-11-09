dbdaeec83371be9364187af37e075178b39991da
Rivero Melany <127607028+MimiRivero@users.noreply.github.com>
2023-11-09 18:16:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,33 @@
+    let url= 'https://opensheet.elk.sh/1uc0Wv37f1Hum4uXOh4bUERMd00WDZD0ByM77V4stHp4/1';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => mostrarData(data))
+    .catch(error => console.log(error))
 
-async function populate() {
-
-  const requestURL = 'https://mimirivero.github.io/pruebajson/json/productos.json';
-  const request = new Request(requestURL);
-
-  const response = await fetch(request);
-  const superHeroesText = await response.text();
-
-  const superHeroes = JSON.parse(superHeroesText);
-  populateHeroes(superHeroes);
-}
-function populateHeroes(obj) {
-  const container_products = document.querySelector('.container-products');
-  const categorias = obj.productos;
-  for (const categoria of categorias) {
-    const div = document.createElement('div');
-    div.classList.add('card-product');
-    const div_imagen = document.createElement('div');
-    div_imagen.classList.add('container-img');
-    div_product = document.createElement('div');
-    div_product.classList.add('content-card-product');
-    div_stars = document.createElement('div');
-    div_stars.classList.add('stars');
-    i_star = document.createElement('i');
-    i_star.classList.add('fa-solid');
-    i_star.classList.add('fa-star');
-    h3_nombre = document.createElement('h3');
-    h3_nombre.classList.add('nombre-producto');
-    h3_nombre.textContent = ` ${categoria.Producto}`;
-    span_carrito = document.createElement('span')
-    span_carrito.classList.add('add-cart')
-    i_carrito = document.createElement('i');
-    i_carrito.classList.add('fa-solid');
-    i_carrito.classList.add('fa-basket-shopping');
-    p_precio = document.createElement('p');
-    p_precio.classList.add('price');
-    p_precio.textContent = ` ${categoria.Precio}`;
-    const crear = document.createElement('img');
-    const imagen = `${categoria.imagen}`;
-    crear.src = imagen;
-
-    container_products.appendChild(div);
-    div.appendChild(div_imagen);
-    div_imagen.appendChild(crear);
-    div.appendChild(div_product);
-    div_product.appendChild(div_stars);
-    div_stars.appendChild(i_star);
-    div_product.appendChild(h3_nombre);
-    div_product.appendChild(span_carrito);
-    span_carrito.appendChild(i_carrito);
-    div_product.appendChild(p_precio);
-  }
-}
-
-populate();
+    const mostrarData = (data) => {
+        console.log(data)
+       let productos = ''
+    for(let i=0; i < data.length; i++){
+        productos+=`<div class="container-products">
+        <div class="card-product">
+            <div class="container-img">
+                <img src="${data[i].imagen}" alt="">
+            </div>
+            <div class="content-card-product">
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <h3 class="nombre-producto">${data[i].Producto}</h3>
+                <span class="add-cart">
+                    <i class="fa-solid fa-basket-shopping"></i>
+                </span>
+                <p class="price">${data[i].Precio}</p>
+            </div>
+        </div>`
+        document.getElementById('container-products').innerHTML = productos
+    }
+    }
