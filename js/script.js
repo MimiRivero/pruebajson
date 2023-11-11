0974e31c24c4ad48451fb8519e82247fb158b870
Rivero Melany <127607028+MimiRivero@users.noreply.github.com>
2023-11-10 22:14:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-    let url= 'https://opensheet.elk.sh/1uc0Wv37f1Hum4uXOh4bUERMd00WDZD0ByM77V4stHp4/1';
-    fetch(url)
+let url = 'https://opensheet.elk.sh/1uc0Wv37f1Hum4uXOh4bUERMd00WDZD0ByM77V4stHp4/1';
+fetch(url)
     .then(response => response.json())
     .then(data => mostrarData(data))
     .catch(error => console.log(error))
-    const mostrarData = (data) => {
-        console.log(data)
-       let productos = ''
-    for(let i=0; i < data.length; i++){
-        productos=`
+const mostrarData = (data) => {
+    console.log(data)
+    let productos = ''
+    for (let i = 0; i < data.length; i++) {
+        productos = `
         <div class="card-product">
+        <a href="html/producto.html?art=${data[i].Art}"> 
             <div class="container-img" style="background-image:url(${data[i].Imagen})"></div>
+        </a>
             <div class="content-card-product">
                 <div class="stars">
                     <i class="fa-solid fa-star"></i>
@@ -18,12 +20,20 @@
                     <i class="fa-solid fa-star"></i>
                     <i class="fa-regular fa-star"></i>
                 </div>
-                <h3 class="nombre-producto">${data[i].Producto}</h3>
+                <h3 class="nombre-producto">        
+                    <a href="html/producto.html?art=${data[i].Art}"> 
+                        ${data[i].Producto}
+                    </a>
+                </h3>
                 <span class="add-cart">
                     <i class="fa-solid fa-basket-shopping"></i>
                 </span>
-                <p class="price">${data[i].Precio}</p>
+                <p class="price">
+                    <a href="html/producto.html?art=${data[i].Art}"> 
+                        ${data[i].Precio}
+                    </a>    
+                </p>
             </div>`
         document.getElementById('container-products').innerHTML += productos
     }
-    }
+}
