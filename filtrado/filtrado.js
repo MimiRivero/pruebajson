5c46b3bf9457c69e82fb9398db36b7a0bcbd8142
Rivero Melany <127607028+MimiRivero@users.noreply.github.com>
2023-11-26 16:40:46 -0300

    Update filtrado.js

--- filtrado/filtrado.js
+++ filtrado/filtrado.js
@@ -1,26 +1,40 @@
-let url= 'https://opensheet.elk.sh/1uc0Wv37f1Hum4uXOh4bUERMd00WDZD0ByM77V4stHp4/1';
+let url = 'https://opensheet.elk.sh/1uc0Wv37f1Hum4uXOh4bUERMd00WDZD0ByM77V4stHp4/1';
 fetch(url)
-.then(response => response.json())
-.then(data => mostrarData(data))
-.catch(error => console.log(error))
+    .then(response => response.json())
+    .then(data => mostrarData(data))
+    .catch(error => console.log(error))
 const mostrarData = (data) => {
     console.log(data)
-   let productos = ''
-for(let i=0; i < data.length; i++){
-    productos=`
-    <div class="products">
-        <div class="container-img" style="background-image:url(${data[i].Imagen})"></div> 
-        <div class="textos">
-            <div class="nom-prod">${data[i].Producto}</div>
-            <div class="precio">${data[i].Precio}</div>
-            <div class="stars">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-regular fa-star"></i>
-        </div>
-    </div>`
-    document.getElementById('section-products').innerHTML += productos
-}
+    let productos = ''
+    for (let i = 0; i < 12; i++) {
+        productos = `    
+        <div class="card-product">
+        <a href="producto.html?art=${data[i].Art}"> 
+            <div class="container-img" style="background-image:url(${data[i].Imagen})"></div>
+        </a>
+            <div class="content-card-product">
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <h3 class="nombre-producto">        
+                    <a href="producto.html?art=${data[i].Art}"> 
+                        ${data[i].Producto}
+                    </a>
+                </h3>
+                <span class="add-cart">
+                    <i class="fa-solid fa-basket-shopping"></i>
+                </span>
+                <p class="price">
+                    <a href="producto.html?art=${data[i].Art}"> 
+                        ${data[i].Precio}
+                    </a>    
+                </p>
+            </div>`
+        
+        document.getElementById('section-products').innerHTML += productos
+    }
 }
